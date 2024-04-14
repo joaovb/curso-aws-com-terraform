61384c1a487bf0b67d833ba21ea00c3e5fcdbb5f
João Victor Barreto <joaovictor.hts@gmail.com>
2024-04-14 02:01:59 +0000

    chore: add files lab hackone and install amb

--- install/install_amb.sh
+++ install/install_amb.sh
@@ -12,6 +12,21 @@ sudo ./aws/install
 # Remove o arquivo zip após a instalação
 rm awscliv2.zip
 
+# Instala o Terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
+
+
 # chmod +x install_amb.sh
 # ./install_amb.sh
 
---
+++ lab-hackone-docker/lab.md
@@ -0,0 +1,6 @@
+### Pegar a URL "localhost" do codespace
+echo ${CODESPACE_NAME}
+
+### Exemplo
+https://opulent-spoon-4jjvxqpw5v737x97-8000.app.github.dev/
+https://opulent-spoon-4jjvxqpw5v737x97-{port_external}.app.github.dev/
